caf9009315ac939351c57d171815739c3a12cf9d
phornchantha <phorn.chantha.2823@rupp.edu.kh>
2025-03-12 11:52:56 +0700

    Create question.js

---
+++ question.js
@@ -0,0 +1,28 @@
+
+const quiz = [
+    {
+        q: "What is 100 + 10 - 10?",
+        options: ["120", "100", "90", "110"],
+        answer: 1
+    },
+    {
+        q: "What is 33 + 33?",
+        options: ["10", "0", "66", "99"],
+        answer: 2
+    },
+    {
+        q: "3 + 7 = 7?",
+        options: ["True", "False"],
+        answer: 1
+    },
+    {
+        q: "What is 44 - 44?",
+        options: ["0", "88", "89"],
+        answer: 0
+    },
+    {
+        q: "What is 2 + 6?",
+        options: ["5", "7", "8", "10"],
+        answer: 2
+    }
+];
